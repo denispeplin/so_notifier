b88b92c9f525c3225c72868e04da2d024495e75d
denispeplin <denis.peplin@gmail.com>
2023-02-05 14:54:55 +0300

    Fetch last 10 questions and print them The solution feels suboptimal since `reqwest` is able to decode JSON without saving it to a String first and using `serde` afterwards. Right now I just don't know how to do that because the root structure contains more than one type of objects, so I would postpone possible optimizations.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+const SO_URL: &str = "https://api.stackexchange.com/2.3/questions?page=1&pagesize=10&order=desc&sort=creation&site=stackoverflow&tagged=";
+// the tag is hardcoded for now to `rust`
+const TAG: &str = "rust";
+
+#[derive(Deserialize, Debug)]
+struct Root {
+    items: Vec<Question>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Question {
+    title: String,
+    link: String,
+    question_id: u32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get(SO_URL.to_owned() + TAG)?.text()?;
+
+    let questions = serde_json::from_str::<Root>(&resp).unwrap();
+    for q in questions.items {
+        println!("{}\n{}\n{}\n", q.title, q.link, q.question_id);
+    }
+
+    Ok(())
 }
