91cc3cf84ba15143228ea20be14f7ff3caf579e0
denispeplin <denis.peplin@gmail.com>
2023-02-05 15:11:18 +0300

    Move fetch-and-print code inside `loop` Also, sleep for one minute after each run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use core::time;
 use serde::Deserialize;
+use std::collections::HashSet;
 
 const SO_URL: &str = "https://api.stackexchange.com/2.3/questions?page=1&pagesize=10&order=desc&sort=creation&site=stackoverflow&tagged=";
 // the tag is hardcoded for now to `rust`
@@ -17,12 +19,14 @@ struct Question {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get(SO_URL.to_owned() + TAG)?.text()?;
+    loop {
+        let resp = reqwest::blocking::get(SO_URL.to_owned() + TAG)?.text()?;
 
-    let questions = serde_json::from_str::<Root>(&resp).unwrap();
-    for q in questions.items {
-        println!("{}\n{}\n{}\n", q.title, q.link, q.question_id);
+        let questions = serde_json::from_str::<Root>(&resp).unwrap();
+        for q in questions.items {
+            println!("{}\n{}\n{}\n", q.title, q.link, q.question_id);
+        }
+
+        std::thread::sleep(time::Duration::from_millis(60_000));
     }
-
-    Ok(())
 }
