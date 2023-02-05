13cf625b9ef76e1174f03398c413cab824dcad9b
denispeplin <denis.peplin@gmail.com>
2023-02-05 22:20:26 +0300

    Don't show the next notification until previous is closed The notification has to be only clicked on, but that leads to its closing, and the next notification is going to be shown. This change is useful because sometimes in one minute more than one new question appears.

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn desktop_notification(summary: &str, link: &str) {
-    Notification::new()
+    let handle = Notification::new()
         .summary(summary)
         .body(link)
         .icon("hint")
         .urgency(Urgency::Critical)
         .show()
         .expect("Notification must be sent");
+
+    // won't show the next notification until the previous one is clicked on
+    handle.wait_for_action(|_action| ());
 }
