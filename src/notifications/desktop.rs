6c195da4d9aeb5b4dd4c1b868f9f6e1114e2bd40
denispeplin <denis.peplin@gmail.com>
2023-02-16 16:14:26 +0300

    Use threads to wait for notification being closed There is no difference in behavior with the previous implementation yet, you'll see it in the next commit

--- src/notifications/desktop.rs
+++ src/notifications/desktop.rs
@@ -1,6 +1,9 @@
 use notify_rust::{Notification, Urgency};
+use std::thread;
 
 pub fn send(summary: &str, link: &str) {
+    let (handle_tx, rx) = std::sync::mpsc::channel();
+
     let handle = Notification::new()
         .summary(summary)
         .body(link)
@@ -9,6 +12,7 @@ pub fn send(summary: &str, link: &str) {
         .show()
         .expect("Notification must be sent");
 
-    // won't show the next notification until the previous one is clicked on
-    handle.wait_for_action(|_action| ());
+    thread::spawn(move || handle.on_close(|_action| handle_tx.send(()).unwrap_or(())));
+
+    rx.recv().unwrap();
 }
